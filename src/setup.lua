fe49f93827ec3ee3c191b8e92de51a77530b1f02
Daniel R. Hunter <drcdh@users.noreply.github.com>
2026-01-13 22:48:21 -0500

    Luarocks (#1)
    luarocks for project management

---
+++ src/setup.lua
@@ -0,0 +1,11 @@
+local version = _VERSION:match("%d+%.%d+")
+
+print("Running setup.lua with _VERSION=".. version)
+
+package.path = '../lua_modules/share/lua/' .. version ..
+  '/?.lua;../lua_modules/share/lua/' .. version ..
+  '/?/init.lua;' .. package.path
+
+package.cpath = '../lua_modules/lib/lua/' .. version ..
+  '/?.so;' .. package.cpath
+
