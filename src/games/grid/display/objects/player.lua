66b36c761ca0558afa4f608a4338ddcda65fe2de
Daniel R. Hunter <danielrhunter@gmail.com>
2026-01-26 23:04:37 -0500

    WIP

--- src/games/grid/display/objects/player.lua
+++ src/games/grid/display/objects/player.lua
@@ -1,5 +1,6 @@
 local RADIUS = .4 -- relative to CELL_PIXELS
 local T_WAKKA = .5 -- seconds
+local THETA_WAKKA = math.pi/4 -- radians, from middle to edge of mouth
 
 local COLORS = { { .6, 0, 0 }, { .6, .6, 1 }, { 1, 1, .6 }, { .6, 0, .6 } }
 
@@ -9,9 +10,8 @@ local function init(i, j, n)
   return o
 end
 
-local function __draw_mouth(self)
+local function __stencil_mouth(self)
   local di, dj = FACE.inv_calc(self.f)
-  local m = 2 * math.abs(self._mouth - .5)
   local r = RADIUS * CELL_PIXELS
   if di ~= 0 then
     love.graphics.polygon("fill", 0, 0, di * r, r * m, di * r, -r * m)
@@ -20,15 +20,23 @@ local function __draw_mouth(self)
   end
 end
 
+local function _draw_beak(self, th)
+  local r = RADIUS * CELL_PIXELS
+  local x, y = r*math.cos(th), r*math.sin(th)
+  love.graphics.circle("fill", x, y, r*.1)
+end
+
 local function _draw(self)
+  local th = THETA_WAKKA * (1 - 2 * math.abs(self._mouth - .5))
   love.graphics.setColor(unpack(self.c))
   love.graphics.stencil(
       function()
-        return __draw_mouth(self)
+        return __stencil_mouth(self)
       end, "increment"
   )
   love.graphics.setStencilTest("less", 1)
   love.graphics.circle("fill", 0, 0, RADIUS * CELL_PIXELS)
+  _draw_beak(self, th)
   love.graphics.setStencilTest()
 end
 
